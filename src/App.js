f2b6a8d29db8c79d3366b43f02a6a10d8fb6e309
NamgyungKim <namgyung.kim@gmail.com>
2021-10-08 16:15:25 +0900

    Feat: Building the Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react"
+import { HashRouter, Route } from "react-router-dom"
+import Home from "./routes/Home"
+import About from "./routes/About"
 
 function App(){
-  return <span>Test</span>
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={ Home } />
+      <Route path="/About" component={ About } />
+    </HashRouter>
+  )
+    
 }
 
 export default App
